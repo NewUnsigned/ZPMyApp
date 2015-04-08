e7ead5f98fa8294eb469def62c02c612891b701f
zhaopeng <zhaopeng0708@icloud.com>
2015-04-09 00:08:46 +0800

    添加AFNetworking框架和SDWebImage框架

--- WeiBo/Classes/Main/View/ZPTabBar.m
+++ WeiBo/Classes/Main/View/ZPTabBar.m
@@ -13,6 +13,54 @@
 @end
 
 @implementation ZPTabBar
+
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    
+    NSInteger index      = 0;
+    CGFloat   barButtonX = 0;
+    CGFloat   barButtonY = 0;
+    CGFloat   barButtonW = self.frame.size.width / 5;
+    CGFloat   barButtonH = self.frame.size.height;
+    
+    for (UIView *subview in self.subviews) {
+        if ([subview isKindOfClass:NSClassFromString(@"UITabBarButton")]) {
+            if (index == 2) {
+                barButtonX = index * barButtonW;
+                self.plusBtn.frame = CGRectMake(barButtonX,barButtonY,barButtonW,barButtonH);
+                index++;
+            }
+            barButtonX    = index * barButtonW;
+            subview.frame = CGRectMake(barButtonX,barButtonY,barButtonW,barButtonH);
+            index++;
+        }
+    }
+}
+
+#pragma lazy
+- (UIButton *)plusBtn
+{
+    if (!_plusBtn) {
+        // 1.创建加号按钮
+        _plusBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_plusBtn addTarget:self action:@selector(compose) forControlEvents:UIControlEventTouchUpInside];
+        // 2.设置按钮的image图片
+        [_plusBtn setImage:[UIImage imageNamed:@"tabbar_compose_icon_add"] forState:UIControlStateNormal];
+        [_plusBtn setImage:[UIImage imageNamed:@"tabbar_compose_icon_add_highlighted"] forState:UIControlStateHighlighted];
+        // 3.设置按钮的背景Image图片
+        [_plusBtn setBackgroundImage:[UIImage imageNamed:@"tabbar_compose_button"] forState:UIControlStateNormal];
+        [_plusBtn setBackgroundImage:[UIImage imageNamed:@"tabbar_compose_button_highlighted"] forState:UIControlStateHighlighted];
+        // 添加到父控件
+        [self addSubview:_plusBtn];
+    }
+    return _plusBtn;
+}
+
+- (void)compose
+{
+    NSLog(@"%s",__func__);
+}
+/*
 {
     CGFloat barButtonW ;
     CGFloat barButtonY ;
@@ -41,34 +89,5 @@
     view.frame = CGRectMake(barButtonX,barButtonY,barButtonW,barButtonH);
     index++;
 }
-
-#pragma lazy
-- (UIButton *)plusBtn
-{
-    if (!_plusBtn) {
-        // 1.创建加号按钮
-        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-        
-        [btn addTarget:self action:@selector(compose) forControlEvents:UIControlEventTouchUpInside];
-        
-        // 2.设置按钮的image图片
-        [btn setImage:[UIImage imageNamed:@"tabbar_compose_icon_add"] forState:UIControlStateNormal];
-        [btn setImage:[UIImage imageNamed:@"tabbar_compose_icon_add_highlighted"] forState:UIControlStateHighlighted];
-        
-        // 3.设置按钮的背景Image图片
-        [btn setBackgroundImage:[UIImage imageNamed:@"tabbar_compose_button"] forState:UIControlStateNormal];
-        [btn setBackgroundImage:[UIImage imageNamed:@"tabbar_compose_button_highlighted"] forState:UIControlStateHighlighted];
-        
-        // 添加到父控件
-        [self addSubview:btn];
-        _plusBtn = btn;
-    }
-    return _plusBtn;
-}
-
-- (void)compose
-{
-    NSLog(@"%s",__func__);
-}
-
+*/
 @end
